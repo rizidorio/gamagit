40c3fffd5effdf8dad0790e93977a9f7830617f7
Ricardo izidorio Ferreira <42355541+rizidorio@users.noreply.github.com>
2020-05-21 18:25:28 -0300

    gamagit
    React

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+import * as S from "./styled.js";
+import { useHistory } from 'react-router-dom';
+
+export default function Repositories() {
+    const [repositories, setRepositories] = useState([]);
+    const history = useHistory();
+
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        if(repositoriesName != null){
+            repositoriesName = JSON.parse(repositoriesName);
+            setRepositories(repositoriesName);
+            localStorage.clear();
+        } else {
+            history.push('/');
+        }
+    }, []);
+
+    return (
+        <S.Container>
+            <S.Title>Repositorios</S.Title>
+            <S.List>
+                { repositories.map(repository => {
+                    return (
+                        <S.ListItem>Repositório: {repository}</S.ListItem>
+                    )
+                })}  
+            </S.List>
+            <S.LinkHome to="/">Voltar</S.LinkHome>
+        </S.Container>
+    )
+}
